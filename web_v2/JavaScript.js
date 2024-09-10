bc1f1ca5361f705ef23582cf69be48292ca16b31
My Duck <My Duck@DESKTOP-7SIVJ8M>
2024-09-10 23:34:16 +0700

    update web v2

---
+++ web_v2/JavaScript.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    function sendData() {
+        var dai = $('#txtA').val();
+        var rong = $('#txtB').val();
+
+        if (isNaN(dai) || isNaN(rong) ) {
+            alert('Vui lòng nhập giá trị hợp lệ cho hai cạnh.');
+            return;
+        }
+
+        var dataToSend = {
+            chieudai: dai,
+            chieurong : rong,
+        };
+
+        $.ajax({
+            url: "WebForm1.aspx",
+            type: "POST",
+            data: JSON.stringify(dataToSend),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (response) {
+                $('#lblResult').html('Diện tích hình chữ nhật là: ' + response.dientich + '<br>' + 'Chu vi hình chữ nhật là :' + response.chuvi);
+            },
+            error: function (xhr, status, error) {
+                $('#lblResult').text('Có lỗi xảy ra: ' + xhr.responseText);
+            }
+        });
+    }
+
+    $("#btnCalculate").click(function () {
+        sendData();
+    });
+});
---
+++ web_v2/WebForm1.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using Newtonsoft.Json;
+using lib_hinhhoc;
+
+namespace web_v2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.HttpMethod == "POST")
+            {
+                try
+                {
+                    string jsonString = new System.IO.StreamReader(Request.InputStream).ReadToEnd();
+                    var data = JsonConvert.DeserializeObject<Dictionary<string, double>>(jsonString);
+
+                    double dai = data["chieudai"];
+                    double rong = data["chieurong"];
+
+                    hinhchunhat hcn = new hinhchunhat();
+                    hcn.a = dai;
+                    hcn.b = rong;
+                    hcn.Xuly();
+
+                    if (hcn.ok)
+                    {
+                        var result = new
+                        {
+                            chuvi = hcn.chuvi,
+                            dientich = hcn.dientich
+                        };
+
+                        Response.ContentType = "application/json";
+                        Response.Write(JsonConvert.SerializeObject(result));
+                    }
+                    else
+                    {
+                        Response.Write("Lỗi: Chiều dài phải lớn hơn chiều rộng và cả hai phải lớn hơn 0.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("Lỗi: " + ex.Message);
+                }
+            }
+        }
+    }
+}
